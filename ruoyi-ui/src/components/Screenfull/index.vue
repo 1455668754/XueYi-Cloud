40e1b0b40796366fb25c8da630a031923eefc64e
雪忆天堂 <1455668754@qq.com>
2021-05-06 20:54:53 +0800

    UI优化

--- ruoyi-ui/src/components/Screenfull/index.vue
+++ ruoyi-ui/src/components/Screenfull/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <svg-icon :icon-class="isFullscreen?'exit-fullscreen':'fullscreen'" @click="click" />
+    <svg-icon :icon-class="isFullscreen?'xy_narrow':'xy_enlarge'" @click="click" />
   </div>
 </template>
 
