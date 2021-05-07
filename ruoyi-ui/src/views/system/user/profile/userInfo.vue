0b72d3725d001c54f720b6b2afbfd3237f387001
雪忆天堂 <1455668754@qq.com>
2021-05-07 10:19:25 +0800

    取消手机号|邮箱强制填写

--- ruoyi-ui/src/views/system/user/profile/userInfo.vue
+++ ruoyi-ui/src/views/system/user/profile/userInfo.vue
@@ -1,7 +1,7 @@
 <template>
   <el-form ref="form" :model="user" :rules="rules" label-width="80px">
     <el-form-item label="用户昵称" prop="nickName">
-      <el-input v-model="user.nickName"  maxlength="20" show-word-limit/>
+      <el-input v-model="user.nickName" maxlength="20" show-word-limit/>
     </el-form-item>
     <el-form-item label="手机号码" prop="phone">
       <el-input v-model="user.phone" maxlength="11"/>
@@ -27,7 +27,7 @@
 </template>
 
 <script>
-import { updateUserProfile } from "@/api/system/user";
+import {updateUserProfile} from "@/api/system/user";
 
 export default {
   props: {
@@ -40,18 +40,16 @@ export default {
       // 表单校验
       rules: {
         nickName: [
-          { required: true, message: "用户昵称不能为空", trigger: "blur" }
+          {required: true, message: "用户昵称不能为空", trigger: "blur"}
         ],
         email: [
-          { required: true, message: "邮箱地址不能为空", trigger: "blur" },
           {
             type: "email",
             message: "'请输入正确的邮箱地址",
             trigger: ["blur", "change"]
           }
         ],
-        phonenumber: [
-          { required: true, message: "手机号码不能为空", trigger: "blur" },
+        phone: [
           {
             pattern: /^1[3|4|5|6|7|8|9][0-9]\d{8}$/,
             message: "请输入正确的手机号码",
@@ -73,7 +71,7 @@ export default {
     },
     close() {
       this.$store.dispatch("tagsView/delView", this.$route);
-      this.$router.push({ path: "/index" });
+      this.$router.push({path: "/index"});
     }
   }
 };
