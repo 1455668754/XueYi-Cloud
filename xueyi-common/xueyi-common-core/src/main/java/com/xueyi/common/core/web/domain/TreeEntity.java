d2c7d1e11aa2765338eccd90700dd8163b94f6dc
雪忆天堂 <1455668754@qq.com>
2021-05-24 19:11:09 +0800

    优化树结构生成

--- xueyi-common/xueyi-common-core/src/main/java/com/xueyi/common/core/web/domain/TreeEntity.java
+++ xueyi-common/xueyi-common-core/src/main/java/com/xueyi/common/core/web/domain/TreeEntity.java
@@ -6,36 +6,23 @@ import java.util.List;
 /**
  * Tree基类
  * 
- * @author ruoyi
+ * @author xueyi
  */
 public class TreeEntity extends BaseEntity
 {
     private static final long serialVersionUID = 1L;
 
-    /** 父菜单名称 */
-    private String parentName;
-
-    /** 父菜单ID */
+    /** 父菜单Id */
     private Long parentId;
 
-    /** 显示顺序 */
-    private Integer orderNum;
+    /** 父菜单名称 */
+    private String parentName;
 
     /** 祖级列表 */
     private String ancestors;
 
     /** 子部门 */
     private List<?> children = new ArrayList<>();
-
-    public String getParentName()
-    {
-        return parentName;
-    }
-
-    public void setParentName(String parentName)
-    {
-        this.parentName = parentName;
-    }
 
     public Long getParentId()
     {
@@ -47,14 +34,14 @@ public class TreeEntity extends BaseEntity
         this.parentId = parentId;
     }
 
-    public Integer getOrderNum()
+    public String getParentName()
     {
-        return orderNum;
+        return parentName;
     }
 
-    public void setOrderNum(Integer orderNum)
+    public void setParentName(String parentName)
     {
-        this.orderNum = orderNum;
+        this.parentName = parentName;
     }
 
     public String getAncestors()
