9d9317ce0629b26e63842679d414f748b3f8c26d
雪忆天堂 <1455668754@qq.com>
2021-07-13 10:11:39 +0800

    删除无用引用

--- xueyi-ui/main/src/api/system/menu.js
+++ xueyi-ui/main/src/api/system/menu.js
@@ -1,14 +1,5 @@
 import request from '@/utils/request'
 
-// // 查询菜单列表
-// export function listMenu(data) {
-//   return request({
-//     url: '/system/menu/list',
-//     method: 'get',
-//     params: data
-//   })
-// }
-
 // 查询菜单详细
 export function getMenu(data) {
   return request({
