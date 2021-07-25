40217d9c088c79be89c9a26d01f6df3993fd14eb
雪忆天堂 <1455668754@qq.com>
2021-07-25 15:43:40 +0800

    状态码401返回Promise.reject

--- xueyi-ui/administrator/src/utils/request.js
+++ xueyi-ui/administrator/src/utils/request.js
@@ -1,9 +1,9 @@
 import axios from 'axios'
-import { Notification, MessageBox, Message } from 'element-ui'
+import {Notification, MessageBox, Message} from 'element-ui'
 import store from '@/store'
-import { getToken } from '@/utils/auth'
+import {getToken} from '@/utils/auth'
 import errorCode from '@/utils/errorCode'
-import { tansParams } from "@/utils/ruoyi";
+import {tansParams} from "@/utils/ruoyi"
 
 axios.defaults.headers['Content-Type'] = 'application/json;charset=utf-8'
 // 创建axios实例
@@ -23,36 +23,36 @@ service.interceptors.request.use(config => {
   }
   // get请求映射params参数
   if (config.method === 'get' && config.params) {
-    let url = config.url + '?';
+    let url = config.url + '?'
     for (const propName of Object.keys(config.params)) {
-      const value = config.params[propName];
-      var part = encodeURIComponent(propName) + "=";
-      if (value !== null && typeof(value) !== "undefined") {
+      const value = config.params[propName]
+      var part = encodeURIComponent(propName) + "="
+      if (value !== null && typeof (value) !== "undefined") {
         if (typeof value === 'object') {
           for (const key of Object.keys(value)) {
-            let params = propName + '[' + key + ']';
-            var subPart = encodeURIComponent(params) + "=";
-            url += subPart + encodeURIComponent(value[key]) + "&";
+            let params = propName + '[' + key + ']'
+            var subPart = encodeURIComponent(params) + "="
+            url += subPart + encodeURIComponent(value[key]) + "&"
           }
         } else {
-          url += part + encodeURIComponent(value) + "&";
+          url += part + encodeURIComponent(value) + "&"
         }
       }
     }
-    url = url.slice(0, -1);
-    config.params = {};
-    config.url = url;
+    url = url.slice(0, -1)
+    config.params = {}
+    config.url = url
   }
   return config
 }, error => {
-    console.log(error)
-    Promise.reject(error)
+  console.log(error)
+  Promise.reject(error)
 })
 
 // 响应拦截器
 service.interceptors.response.use(res => {
     // 未设置状态码则默认成功状态
-    const code = res.data.code || 200;
+    const code = res.data.code || 200
     // 获取错误信息
     const msg = errorCode[code] || res.data.msg || errorCode['default']
     if (code === 401) {
@@ -63,9 +63,11 @@ service.interceptors.response.use(res => {
         }
       ).then(() => {
         store.dispatch('LogOut').then(() => {
-          location.href = '/index';
+          location.href = '/index'
         })
-      }).catch(() => {});
+      }).catch(() => {
+      })
+      return Promise.reject('error')
     } else if (code === 500) {
       Message({
         message: msg,
@@ -83,15 +85,13 @@ service.interceptors.response.use(res => {
   },
   error => {
     console.log('err' + error)
-    let { message } = error;
+    let {message} = error
     if (message == "Network Error") {
-      message = "后端接口连接异常";
-    }
-    else if (message.includes("timeout")) {
-      message = "系统接口请求超时";
-    }
-    else if (message.includes("Request failed with status code")) {
-      message = "系统接口" + message.substr(message.length - 3) + "异常";
+      message = "后端接口连接异常"
+    } else if (message.includes("timeout")) {
+      message = "系统接口请求超时"
+    } else if (message.includes("Request failed with status code")) {
+      message = "系统接口" + message.substr(message.length - 3) + "异常"
     }
     Message({
       message: message,
@@ -109,7 +109,7 @@ export function download(url, params, filename) {
       return tansParams(params)
     }],
     headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
+      'Content-Type': 'application/x-www-form-urlencoded'
     },
     responseType: 'blob'
   }).then((data) => {
