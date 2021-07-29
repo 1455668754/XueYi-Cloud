29bec831d1efb29be31cc046f191a6b1c5d707db
雪忆天堂 <1455668754@qq.com>
2021-07-29 12:52:46 +0800

    移动端设备检验

--- xueyi-ui/main/src/utils/xueyi.js
+++ xueyi-ui/main/src/utils/xueyi.js
@@ -231,3 +231,8 @@ export function mergeTableRow(config) {
   })
   return data
 }
+
+// 检验是否为移动端设备
+export function isMobile() {
+  return navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i);
+}
