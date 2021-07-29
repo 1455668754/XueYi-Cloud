082862ae8d564d14f87c6cf5479f83c7edb9d8d6
雪忆天堂 <1455668754@qq.com>
2021-07-29 22:04:34 +0800

    新增消息组件

--- xueyi-ui/administrator/src/layout/components/Navbar.vue
+++ xueyi-ui/administrator/src/layout/components/Navbar.vue
@@ -9,6 +9,8 @@
       <template v-if="device!=='mobile'">
         <search id="header-search" class="right-menu-item" />
 
+        <Notice id="head-notice" class="right-menu-item hover-effect"/>
+
         <screenfull id="screenfull" class="right-menu-item hover-effect" />
 
         <el-tooltip content="布局大小" effect="dark" placement="bottom">
@@ -43,6 +45,7 @@ import Hamburger from '@/components/Hamburger'
 import Screenfull from '@/components/Screenfull'
 import SizeSelect from '@/components/SizeSelect'
 import Search from '@/components/HeaderSearch'
+import Notice from '@/components/XyComponents/Notice'
 
 export default {
   components: {
@@ -51,7 +54,8 @@ export default {
     Hamburger,
     Screenfull,
     SizeSelect,
-    Search
+    Search,
+    Notice
   },
   computed: {
     ...mapGetters([
