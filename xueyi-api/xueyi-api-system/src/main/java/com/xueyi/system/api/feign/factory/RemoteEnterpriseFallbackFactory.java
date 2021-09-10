a5d1132eea724edebc60edd4de9044e337970ad9
雪忆天堂 <1455668754@qq.com>
2021-09-10 11:55:01 +0800

    增加企业信息调用接口feign

--- xueyi-api/xueyi-api-system/src/main/java/com/xueyi/system/api/feign/factory/RemoteEnterpriseFallbackFactory.java
+++ xueyi-api/xueyi-api-system/src/main/java/com/xueyi/system/api/feign/factory/RemoteEnterpriseFallbackFactory.java
@@ -1,15 +1,15 @@
 package com.xueyi.system.api.feign.factory;
 
-import com.xueyi.common.core.domain.R;
-import com.xueyi.system.api.domain.organize.SysEnterprise;
-import com.xueyi.system.api.feign.RemoteEnterpriseService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.cloud.openfeign.FallbackFactory;
+import com.xueyi.common.core.domain.R;
 import org.springframework.stereotype.Component;
+import com.xueyi.system.api.domain.organize.SysEnterprise;
+import com.xueyi.system.api.feign.RemoteEnterpriseService;
+import org.springframework.cloud.openfeign.FallbackFactory;
 
 /**
- * 租户服务降级处理
+ * 企业信息服务降级处理
  *
  * @author xueyi
  */
@@ -20,22 +20,11 @@ public class RemoteEnterpriseFallbackFactory implements FallbackFactory<RemoteEn
 
     @Override
     public RemoteEnterpriseService create(Throwable throwable) {
-        log.error("租户服务调用失败:{}", throwable.getMessage());
+        log.error("企业信息服务调用失败:{}", throwable.getMessage());
         return new RemoteEnterpriseService() {
-
             @Override
-            public R<Boolean> refreshEnterpriseCache(SysEnterprise newEnterprise, String source) {
-                return R.fail("企业key新增|更新cache失败:" + throwable.getMessage());
-            }
-
-            @Override
-            public R<Boolean> refreshEnterpriseKey(String oldEnterpriseName, SysEnterprise newEnterprise, String source) {
-                return R.fail("刷新企业cache的key失败:" + throwable.getMessage());
-            }
-
-            @Override
-            public R<Boolean> resetEnterpriseCache(String source) {
-                return R.fail("重置企业缓存失败:" + throwable.getMessage());
+            public R<SysEnterprise> getEnterpriseByEnterpriseId(Long enterpriseId, String source) {
+                return R.fail("获取企业信息失败:" + throwable.getMessage());
             }
         };
     }
