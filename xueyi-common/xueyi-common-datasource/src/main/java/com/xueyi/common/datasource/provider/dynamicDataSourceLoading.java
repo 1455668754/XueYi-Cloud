04045cc8727e06a11cc8cfce500eebf1dc71983c
雪忆天堂 <1455668754@qq.com>
2021-09-10 11:56:30 +0800

    优化初始化源注入字段

--- xueyi-common/xueyi-common-datasource/src/main/java/com/xueyi/common/datasource/provider/dynamicDataSourceLoading.java
+++ xueyi-common/xueyi-common-datasource/src/main/java/com/xueyi/common/datasource/provider/dynamicDataSourceLoading.java
@@ -35,13 +35,13 @@ public class dynamicDataSourceLoading {
         return new AbstractJdbcDataSourceProvider(Driver, Url, UserName, PassWord) {
             @Override
             protected Map<String, DataSourceProperty> executeStmt(Statement statement) throws SQLException {
-                ResultSet rs = statement.executeQuery("select s.slave,s.username,s.password,s.url,s.driver_class_name from xy_tenant_source s where s.status = 0 and s.del_flag = 0");
+                ResultSet rs = statement.executeQuery("select s.slave, s.username, s.password, s.url_prepend, s.url_append, s.driver_class_name from xy_tenant_source s where s.status = 0 and s.del_flag = 0");
                 Map<String, DataSourceProperty> map = new HashMap<String, DataSourceProperty>();
                 while (rs.next()) {
                     String name = rs.getString("slave");
                     String username = rs.getString("username");
                     String password = rs.getString("password");
-                    String url = rs.getString("url");
+                    String url = rs.getString("url_prepend").concat(rs.getString("url_append"));
                     String driver = rs.getString("driver_class_name");
                     DataSourceProperty property = new DataSourceProperty();
                     property.setUsername(username);
