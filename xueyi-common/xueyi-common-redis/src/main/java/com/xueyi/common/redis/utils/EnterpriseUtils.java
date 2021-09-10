ad0b38ca2b183c75c8bf048613fec30e297e0bcd
雪忆天堂 <1455668754@qq.com>
2021-09-10 11:55:49 +0800

    增加企业redis-key逻辑工具

---
+++ xueyi-common/xueyi-common-redis/src/main/java/com/xueyi/common/redis/utils/EnterpriseUtils.java
@@ -0,0 +1,36 @@
+package com.xueyi.common.redis.utils;
+
+import com.xueyi.common.core.constant.Constants;
+
+public class EnterpriseUtils {
+
+    /**
+     * 获取企业Id key
+     *
+     * @param enterpriseName 企业账号
+     * @return 缓存键key
+     */
+    public static String getLoginCacheKey(String enterpriseName) {
+        return Constants.LOGIN_ENTERPRISE_KEY + enterpriseName;
+    }
+
+    /**
+     * 获取企业信息 key
+     *
+     * @param enterpriseId 企业Id
+     * @return 缓存键key
+     */
+    public static String getEnterpriseCacheKey(Long enterpriseId) {
+        return Constants.SYS_ENTERPRISE_KEY + enterpriseId + ":" + Constants.ENTERPRISE_KEY;
+    }
+
+    /**
+     * 获取源策略Id key
+     *
+     * @param enterpriseId 企业Id
+     * @return 缓存键key
+     */
+    public static String getStrategyCacheKey(Long enterpriseId) {
+        return Constants.SYS_ENTERPRISE_KEY + enterpriseId + ":" + Constants.ENTERPRISE_KEY;
+    }
+}
