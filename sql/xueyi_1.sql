f29ccf6d72450cb107e875e28e77cfe3f80cdbaf
雪忆天堂 <1455668754@qq.com>
2021-10-27 20:30:26 +0800

    移除默认数据源信息（nacos连接时已加载）

--- sql/xueyi_1.sql
+++ sql/xueyi_1.sql
@@ -87,8 +87,7 @@ primary key (source_id)
 -- 初始化-数据源表数据 | 这条数据为我的基础库，实际使用时调整成自己的库即可
 -- ----------------------------
 insert into xy_tenant_source(source_id, name, database_type, is_change, slave, driver_class_name, url_prepend, url_append, username, password, type)
-values (0, '默认数据源', '1', 'Y', 'master', 'com.mysql.cj.jdbc.Driver', 'jdbc:mysql://localhost:3306/xy-cloud', '?useUnicode=true&characterEncoding=utf8&zeroDateTimeBehavior=convertToNull&useSSL=true&serverTimezone=GMT%2B8', 'root', 'password', '0'),
-       (1, '注册数据源', '0', 'Y', 'slave', 'com.mysql.cj.jdbc.Driver', 'jdbc:mysql://localhost:3306/xy-cloud', '?useUnicode=true&characterEncoding=utf8&zeroDateTimeBehavior=convertToNull&useSSL=true&serverTimezone=GMT%2B8', 'root', 'password', '0');
+values (1, '注册数据源', '0', 'Y', 'slave', 'com.mysql.cj.jdbc.Driver', 'jdbc:mysql://localhost:3306/xy-cloud', '?useUnicode=true&characterEncoding=utf8&zeroDateTimeBehavior=convertToNull&useSSL=true&serverTimezone=GMT%2B8', 'root', 'password', '0');
 
 -- ----------------------------
 -- 4、主从库关联表  写1-n读
