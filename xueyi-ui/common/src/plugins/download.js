a951bfb95c4cb9508db3284275418daac462db70
奇点 <1455668754@qq.com>
2021-12-15 09:11:48 +0800

    优化下载解析blob异常提示

--- xueyi-ui/common/src/plugins/download.js
+++ xueyi-ui/common/src/plugins/download.js
@@ -1,8 +1,9 @@
 import axios from '~../../axios'
-import { Message } from '~../../element-ui'
-import { saveAs } from '~../../file-saver'
-import { getToken } from '../utils/auth'
-import { blobValidate } from "../utils/ruoyi";
+import {Message} from '~../../element-ui'
+import {saveAs} from '~../../file-saver'
+import {getToken} from '../utils/auth'
+import errorCode from '../utils/errorCode'
+import {blobValidate} from "../utils/ruoyi"
 
 const baseURL = process.env.VUE_APP_BASE_API
 
@@ -13,18 +14,24 @@ export default {
             method: 'get',
             url: url,
             responseType: 'blob',
-            headers: { 'Authorization': 'Bearer ' + getToken() }
+            headers: {'Authorization': 'Bearer ' + getToken()}
         }).then(async (res) => {
-            const isLogin = await blobValidate(res.data);
+            const isLogin = await blobValidate(res.data)
             if (isLogin) {
-                const blob = new Blob([res.data], { type: 'application/zip' })
+                const blob = new Blob([res.data], {type: 'application/zip'})
                 this.saveAs(blob, name)
             } else {
-                Message.error('无效的会话，或者会话已过期，请重新登录。');
+                this.printErrMsg(res.data)
             }
         })
     },
     saveAs(text, name, opts) {
-        saveAs(text, name, opts);
+        saveAs(text, name, opts)
+    },
+    async printErrMsg(data) {
+        const resText = await data.text()
+        const rspObj = JSON.parse(resText)
+        const errMsg = errorCode[rspObj.code] || rspObj.msg || errorCode['default']
+        Message.error(errMsg)
     }
 }
--- xueyi-ui/common/src/utils/request.js
+++ xueyi-ui/common/src/utils/request.js
@@ -66,41 +66,39 @@ service.interceptors.response.use(
                 })
             }).catch(() => {
             })
-        return Promise.reject("无效的会话，或者会话已过期，请重新登录。")
+            return Promise.reject("无效的会话，或者会话已过期，请重新登录。")
+        } else if (code === 500) {
+            Message({
+                message: msg,
+                type: "error"
+            })
+            return Promise.reject(new Error(msg))
+        } else if (code !== 200) {
+            Notification.error({
+                title: msg
+            })
+            return Promise.reject("error")
+        } else {
+            return res.data
+        }
+    },
+    (error) => {
+        console.log("err" + error)
+        let {message} = error
+        if (message == "Network Error") {
+            message = "后端接口连接异常"
+        } else if (message.includes("timeout")) {
+            message = "系统接口请求超时"
+        } else if (message.includes("Request failed with status code")) {
+            message = "系统接口" + message.substr(message.length - 3) + "异常"
+        }
+        Message({
+            message: message,
+            type: "error",
+            duration: 5 * 1000
+        })
+        return Promise.reject(error)
     }
-else
-if (code === 500) {
-    Message({
-        message: msg,
-        type: "error"
-    })
-    return Promise.reject(new Error(msg))
-} else if (code !== 200) {
-    Notification.error({
-        title: msg
-    })
-    return Promise.reject("error")
-} else {
-    return res.data
-}
-},
-(error) => {
-    console.log("err" + error)
-    let {message} = error
-    if (message == "Network Error") {
-        message = "后端接口连接异常"
-    } else if (message.includes("timeout")) {
-        message = "系统接口请求超时"
-    } else if (message.includes("Request failed with status code")) {
-        message = "系统接口" + message.substr(message.length - 3) + "异常"
-    }
-    Message({
-        message: message,
-        type: "error",
-        duration: 5 * 1000
-    })
-    return Promise.reject(error)
-}
 )
 
 
@@ -127,7 +125,10 @@ export function download(url, params, filename) {
                 const blob = new Blob([data])
                 saveAs(blob, filename)
             } else {
-                Message.error("无效的会话，或者会话已过期，请重新登录。")
+                const resText = await data.text()
+                const rspObj = JSON.parse(resText)
+                const errMsg = errorCode[rspObj.code] || rspObj.msg || errorCode['default']
+                Message.error(errMsg)
             }
             downloadLoadingInstance.close()
         })
