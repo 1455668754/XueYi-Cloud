49c1f1bc894d730052df1602d4746ca4736ffa1a
奇点 <1455668754@qq.com>
2022-04-12 11:12:19 +0800

    修正代码生成权限常量标识问题

--- xueyi-modules/xueyi-system/src/main/java/com/xueyi/system/notice/service/impl/SysNoticeServiceImpl.java
+++ xueyi-modules/xueyi-system/src/main/java/com/xueyi/system/notice/service/impl/SysNoticeServiceImpl.java
@@ -1,11 +1,14 @@
 package com.xueyi.system.notice.service.impl;
 
+import com.xueyi.common.datascope.annotation.DataScope;
 import com.xueyi.common.web.entity.service.impl.BaseServiceImpl;
 import com.xueyi.system.notice.domain.dto.SysNoticeDto;
 import com.xueyi.system.notice.manager.SysNoticeManager;
 import com.xueyi.system.notice.mapper.SysNoticeMapper;
 import com.xueyi.system.notice.service.ISysNoticeService;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * 通知公告管理 服务层处理
@@ -14,4 +17,16 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class SysNoticeServiceImpl extends BaseServiceImpl<SysNoticeDto, SysNoticeManager, SysNoticeMapper> implements ISysNoticeService {
+
+    /**
+     * 查询通知公告对象列表 | 数据权限 | 附加数据
+     *
+     * @param notice 通知公告对象
+     * @return 通知公告对象集合
+     */
+    @Override
+    @DataScope(userAlias = "createBy", mapperScope = {"SysNoticeMapper"})
+    public List<SysNoticeDto> selectListScope(SysNoticeDto notice) {
+        return baseManager.selectListExtra(notice);
+    }
 }
