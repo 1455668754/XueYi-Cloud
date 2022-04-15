e89993b93edfaf054543d0e7c99dfb0dbf5fc343
雪忆天堂 <1455668754@qq.com>
2022-04-15 10:02:54 +0000

    字典类型必须以字母开头，且只能为（小写字母，数字，下滑线）

--- xueyi-api/xueyi-api-system/src/main/java/com/xueyi/system/api/dict/domain/po/SysDictTypePo.java
+++ xueyi-api/xueyi-api-system/src/main/java/com/xueyi/system/api/dict/domain/po/SysDictTypePo.java
@@ -5,6 +5,10 @@ import com.baomidou.mybatisplus.annotation.TableField;
 import com.xueyi.common.core.annotation.Excel;
 import com.xueyi.common.core.web.entity.base.SubBaseEntity;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 /**
  * 字典类型 持久化对象
  *
@@ -20,11 +24,21 @@ public class SysDictTypePo<S> extends SubBaseEntity<S> {
     @TableField(value = "code", updateStrategy = FieldStrategy.NEVER)
     private String code;
 
+    @NotBlank(message = "字典类型不能为空")
+    @Size(min = 0, max = 100, message = "字典类型类型长度不能超过100个字符")
+    @Pattern(regexp = "^[a-z][a-z0-9_]*$", message = "字典类型必须以字母开头，且只能为（小写字母，数字，下滑线）")
     public String getCode() {
         return code;
     }
 
     public void setCode(String code) {
         this.code = code;
+    }
+
+    @Override
+    @NotBlank(message = "字典名称不能为空")
+    @Size(min = 0, max = 100, message = "字典名称长度不能超过100个字符")
+    public String getName() {
+        return super.getName();
     }
 }
