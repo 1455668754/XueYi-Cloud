002b2ff85ebf677c487e7611580368a980d61333
雪忆天堂 <1455668754@qq.com>
2022-06-19 11:37:42 +0800

    回退fastjson2版本，修正redis反序列化问题

--- xueyi-common/xueyi-common-redis/src/main/java/com/xueyi/common/redis/configure/FastJson2JsonRedisSerializer.java
+++ xueyi-common/xueyi-common-redis/src/main/java/com/xueyi/common/redis/configure/FastJson2JsonRedisSerializer.java
@@ -15,10 +15,10 @@ import java.nio.charset.StandardCharsets;
  * @author xueyi
  */
 public class FastJson2JsonRedisSerializer<T> implements RedisSerializer<T> {
+
     public static final Charset DEFAULT_CHARSET = StandardCharsets.UTF_8;
 
     private Class<T> clazz;
-
 
     public FastJson2JsonRedisSerializer(Class<T> clazz) {
         super();
@@ -35,11 +35,9 @@ public class FastJson2JsonRedisSerializer<T> implements RedisSerializer<T> {
 
     @Override
     public T deserialize(byte[] bytes) throws SerializationException {
-        if (bytes == null || bytes.length <= 0) {
+        if (bytes == null || bytes.length <= 0)
             return null;
-        }
         String str = new String(bytes, DEFAULT_CHARSET);
-
         return JSON.parseObject(str, clazz, JSONReader.Feature.SupportAutoType);
     }
 }
